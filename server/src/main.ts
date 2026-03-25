42d5359ac6a7f8645c200a70d415bca2e145142f
Makrad0r <asafovp1@gmail.com>
2026-03-25 13:24:25 +0300

    prepare for railway

--- server/src/main.ts
+++ server/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Server running on http://localhost:${port}`);
 }
 bootstrap();
